d396ef49d431efa594df34c9df924b51782c8e21
LutziGoz <45577616+LutziGoz@users.noreply.github.com>
2021-12-15 14:13:07 +0200

    Create radio 24-7 project main.py

--- radio 24-7 project main.py
+++ radio 24-7 project main.py
@@ -23,21 +23,21 @@ def talk (url):
         engine.say("Great, enjoy")
         engine.runAndWait()
         webbrowser.open(url)
-genres = {'Vintage radio': "https://www.youtube.com/watch?v=tb0B3auGbtA",
+genres = {'Vintage radio': "https://www.youtube.com/watch?v=kxf5P0ejS9s",
           'Electronic': "https://www.youtube.com/watch?v=thd6h-ZZIfo",
-          'dance music': 'https://www.youtube.com/watch?v=YSBO7Zl8mU4',
+          'dance music': 'https://www.youtube.com/watch?v=ARbjFgb-URw',
           'Hip-hop': 'https://www.youtube.com/watch?v=L9Q1HUdUMp0',
-          'Indie': 'https://www.youtube.com/watch?v=oVi5gtzTDx0',
-          'rock': 'https://www.youtube.com/watch?v=nwYAt3-IPHY',
+          'Indie': 'https://www.youtube.com/watch?v=qi3Hfy8waFo',
+          'rock': 'https://www.youtube.com/watch?v=OPYwHUYjQWc',
           'Jazz': 'https://www.youtube.com/watch?v=Dx5qFachd3A',
-          'blues': 'https://www.youtube.com/watch?v=UX8bHFoJyeY',
+          'blues': 'https://www.youtube.com/watch?v=pT9RPH-ga9w',
           'Metal': 'https://www.youtube.com/watch?v=dGfdGZ8cH-o',
-          'Oldies': 'https://www.youtube.com/watch?v=tb0B3auGbtA',
-          'pop': 'https://www.youtube.com/watch?v=kGKkUN50R0c',
+          'Fallout Radio': 'https://www.youtube.com/watch?v=Ya3WXzEBL1E',
+          'reggae': 'https://www.youtube.com/watch?v=jBLapoJrkf8',
           'Rap': 'https://www.youtube.com/watch?v=05689ErDUdM',
-          'R&B': 'https://www.youtube.com/watch?v=L9Q1HUdUMp0',
-          'drum&bass': 'https://www.youtube.com/watch?v=NXX71vBzqtc',
-          'Trance' : 'https://www.youtube.com/watch?v=4ihJ_mA8Fsk' ,
+          'Minimal_Techno': 'https://www.youtube.com/watch?v=n0Zli_fQeP8',
+          'drum&bass': 'https://www.youtube.com/watch?v=xevHCoYIXus',
+          'Trance' : 'https://www.youtube.com/watch?v=839IFrk-Xig' ,
           'telegram MetalBot':'https://t.me/NewsMetaLutzi',
           'donate': 'https://www.paypal.com/donate?hosted_button_id=Q7JBNHULDUQ74',
           'fallout radio' : 'https://www.youtube.com/watch?v=tzBGEqkwCoY'}
@@ -54,14 +54,14 @@ b2 = Radiobutton(root, variable=v, value=genres.get("Electronic"), text='2. Elec
 b3 = Radiobutton(root, variable=v, value=genres.get('Dance Music'), text='3. Dance Music', font=f1, bg=bg1, command=lambda aurl=genres.get('dance music'): talk(aurl)).grid(row=4, column=0, sticky=W)
 b4 = Radiobutton(root, variable=v, value=genres.get('Hip-Hop'), text='4. Hip-Hop', font=f1, bg=bg1, command=lambda aurl=genres.get('Hip-hop'): talk(aurl)).grid(row=5, column=0, sticky=W)
 b5 = Radiobutton(root, variable=v, value=genres.get('Indie'), text='5.Indie', font=f1, bg=bg1, command=lambda aurl=genres.get('Indie'): talk(aurl)).grid(row=6, column=0, sticky=W)
-b6 = Radiobutton(root, variable=v, value=genres.get('Rock and Roll'), text='6. Rock and Roll', font=f1, bg=bg1, command=lambda aurl=genres.get('rock'): talk(aurl)).grid(row=7, column=0, sticky=W)
+b6 = Radiobutton(root, variable=v, value=genres.get('Classic Rock'), text='6. Rock and Roll', font=f1, bg=bg1, command=lambda aurl=genres.get('rock'): talk(aurl)).grid(row=7, column=0, sticky=W)
 b7 = Radiobutton(root, variable=v, value=genres.get('Jazz'), text='7.Jazz', font=f1, bg=bg1, command=lambda aurl=genres.get('Jazz'): talk(aurl)).grid(row=8, column=0, sticky=W)
 b8 = Radiobutton(root, variable=v, value=genres.get('blues'), text='8. blues', font=f1, bg=bg1, command=lambda aurl=genres.get('blues'): talk(aurl)).grid(row=9, column=0, sticky=W)
 b9 = Radiobutton(root, variable=v, value=genres.get('Metal'), text='9. Metal', font=f1, bg=bg1, command=lambda aurl=genres.get('Metal'): talk(aurl)).grid(row=10, column=0, sticky=W)
-b10 = Radiobutton(root, variable=v, value=genres.get('Oldies'), text='10. Oldies', font=f1, bg=bg1, command=lambda aurl=genres.get('pop'): talk(aurl)).grid(row=11, column=0, sticky=W)
-b11 = Radiobutton(root, variable=v, value=genres.get('Pop'), text='11. Pop', font=f1, bg=bg1, command = lambda aurl=genres.get('Rap'): talk(aurl)).grid(row=12, column=0, sticky=W)
+b10 = Radiobutton(root, variable=v, value=genres.get('Fallout Radio'), text='10. Fallout Radio', font=f1, bg=bg1, command=lambda aurl=genres.get('Fallout Radio'): talk(aurl)).grid(row=11, column=0, sticky=W)
+b11 = Radiobutton(root, variable=v, value=genres.get('Reggae'), text='11. Reggae', font=f1, bg=bg1, command = lambda aurl=genres.get('reggae'): talk(aurl)).grid(row=12, column=0, sticky=W)
 b12 = Radiobutton(root, variable=v, value=genres.get('Rap'), text='12. Rap', font=f1, bg=bg1,command = lambda aurl=genres.get('Rap'): talk(aurl)).grid(row=13, column=0, sticky=W)
-b13 = Radiobutton(root, variable=v, value=genres.get('Rhythm & Blues'), text='13. Rhythm & Blues', font=f1, bg=bg1, command=lambda aurl=genres.get('R&B'): talk(aurl)).grid(row=14, column=0, sticky=W)
+b13 = Radiobutton(root, variable=v, value=genres.get('Minimal Techno'), text='13. Minimal Techno', font=f1, bg=bg1, command=lambda aurl=genres.get('Minimal_Techno'): talk(aurl)).grid(row=14, column=0, sticky=W)
 b14 = Radiobutton(root, variable=v, value=genres.get('Drum and Bass'), text='14. Drum and Bass', font=f1, bg=bg1, command=lambda aurl=genres.get('drum&bass'): talk(aurl)).grid(row=15, column=0, sticky=W)
 b15 = Radiobutton(root, variable=v, value=genres.get('Trance'), text='14. Trance-Goa+Dark+Psy', font=f1, bg=bg1, command=lambda aurl=genres.get('Trance'): talk(aurl)).grid(row=16, column=0, sticky=W)
 b16 = Radiobutton(root, variable=v, value=genres.get('telegram MetalBot'), text='16. telegram-Metal-Bot~~Daily-News~~', font=f1, bg=bg3, command=lambda aurl=genres.get('telegram MetalBot'): talk(aurl)).grid(row=14, column=1, sticky=W)
